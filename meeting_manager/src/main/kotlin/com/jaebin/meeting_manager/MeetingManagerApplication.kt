bc79549cef657c201580564e96ed644ce1339b68
jaebin <ljbfif50@gmail.com>
2022-09-19 10:26:09 +0900

    init project

---
+++ meeting_manager/src/main/kotlin/com/jaebin/meeting_manager/MeetingManagerApplication.kt
@@ -0,0 +1,11 @@
+package com.jaebin.meeting_manager
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MeetingManagerApplication
+
+fun main(args: Array<String>) {
+    runApplication<MeetingManagerApplication>(*args)
+}
---
+++ meeting_manager/src/test/kotlin/com/jaebin/meeting_manager/MeetingManagerApplicationTests.kt
@@ -0,0 +1,12 @@
+package com.jaebin.meeting_manager
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MeetingManagerApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+}
