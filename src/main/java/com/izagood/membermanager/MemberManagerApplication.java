f9a14656feded8be2ed40186826f67132ccd4ca7
izagood <ljbfif50@gmail.com>
2022-12-28 02:09:05 +0900

    프로젝트를 Kotlin 에서 Java 로 변경

---
+++ src/main/java/com/izagood/membermanager/MemberManagerApplication.java
@@ -0,0 +1,13 @@
+package com.izagood.membermanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemberManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MemberManagerApplication.class, args);
+    }
+
+}
